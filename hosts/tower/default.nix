74677574572d9249c7422cd54329c2b81c2dc4da
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-17 17:31:30 +0100

    completely removes bootloader

--- hosts/tower/default.nix
+++ hosts/tower/default.nix
@@ -17,12 +17,12 @@
   boot.supportedFilesystems = [ "ntfs" ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.useOSProber = true;
+  # boot.loader.systemd-boot.enable = false;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "nodev";
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.useOSProber = true;
 
   # this sets the system time to local time to work with windows dual boot
   time.hardwareClockInLocalTime = true;
