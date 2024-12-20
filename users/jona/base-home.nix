3d3fb450f8b9970c64b163a5ea9369e6e8c6118f
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-20 03:55:51 +0100

    removes unused packages

--- users/jona/base-home.nix
+++ users/jona/base-home.nix
@@ -19,28 +19,28 @@
 
     # archives
     zip
-    xz
+    # xz
     unzip
 
     # utils
     ripgrep # recursively searches directories for a regex pattern
     jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
+    # yq-go # yaml processor https://github.com/mikefarah/yq
+    # eza # A modern replacement for ‘ls’
     fzf # A command-line fuzzy finder
 
     # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc # it is a calculator for the IPv4/v6 addresses
+    # mtr # A network diagnostic tool
+    # iperf3
+    # dnsutils # `dig` + `nslookup`
+    # ldns # replacement of `dig`, it provide the command `drill`
+    # aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    # socat # replacement of openbsd-netcat
+    # nmap # A utility for network discovery and security auditing
+    # ipcalc # it is a calculator for the IPv4/v6 addresses
 
     # misc
-    cowsay
+    # cowsay
     file
     which
     tree
@@ -48,17 +48,17 @@
     gnutar
     gawk
     zstd
-    gnupg
+    # gnupg
 
     # nix related
     #
     # it provides the command `nom` works just like `nix`
     # with more details log output
-    nix-output-monitor
+    # nix-output-monitor
 
     # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
+    # hugo # static site generator
+    # glow # markdown previewer in terminal
 
     btop # replacement of htop/nmon
     iotop # io monitoring
