1ed7cebc60cbeb3cf450bfe238007de0e847c62c
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-20 02:45:00 +0100

    uncomment sway options

--- home/programs/sway/default.nix
+++ home/programs/sway/default.nix
@@ -20,6 +20,7 @@
   home.packages = with pkgs; [
     rofi-wayland
     pcmanfm
+      sway
   ];
   # programs.sway = {
   #   enable = true;
@@ -28,26 +29,25 @@
   programs.sway = {
     enable = true;
     # systemd.enable = true;
-    wrapperFeatures.gtk = true;
-    extraPackages = with pkgs; [
-      mako
-      kitty
-      foot
-      sway
-      wayland
-      xdg-utils
-      glib
-      grim
-      slurp
-      wl-clipboard
-    ];
-
-    extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
-      export QT_QPA_PLATFORM=wayland
-      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
-      export _JAVA_AWT_WM_NONREPARENTING=1
-      export MOZ_ENABLE_WAYLAND=1
-    '';
-  };
+  #   wrapperFeatures.gtk = true;
+  #   extraPackages = with pkgs; [
+  #     mako
+  #     kitty
+  #     foot
+  #     wayland
+  #     xdg-utils
+  #     glib
+  #     grim
+  #     slurp
+  #     wl-clipboard
+  #   ];
+  #
+  #   extraSessionCommands = ''
+  #     export SDL_VIDEODRIVER=wayland
+  #     export QT_QPA_PLATFORM=wayland
+  #     export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+  #     export _JAVA_AWT_WM_NONREPARENTING=1
+  #     export MOZ_ENABLE_WAYLAND=1
+  #   '';
+  # };
 }
