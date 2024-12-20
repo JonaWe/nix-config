9bf3dc972389e88f0fd30b0a8867572d23425a77
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-20 01:49:22 +0100

    adds kitty and zathura to sway user

--- users/jona/sway-home.nix
+++ users/jona/sway-home.nix
@@ -7,6 +7,8 @@
     ../../home/programs/neovim
     ../../home/programs/tmux
     ../../home/programs/sway
+    ../../home/programs/kitty
+    ../../home/programs/zathura
     ./base-home.nix
   ];
 
