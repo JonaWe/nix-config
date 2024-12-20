e312a50b7e324147be310f48df7db4b879db25b8
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-20 02:11:55 +0100

    adds rofi

--- home/programs/sway/default.nix
+++ home/programs/sway/default.nix
@@ -12,7 +12,12 @@
     source = ./waybar;
     recursive = true;
   };
+  home.file.".config/rofi" = {
+    source = ./rofi;
+    recursive = true;
+  };
   programs.waybar.enable = true;
+  programs.rofy-wayland.enable = true;
   # programs.sway = {
   #   enable = true;
   # };
