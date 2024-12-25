b498e3b70b815dcd45abae03537181ebd623609e
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-25 21:50:19 +0100

    adds gvfs and tumbler

--- hosts/tower/default.nix
+++ hosts/tower/default.nix
@@ -16,6 +16,9 @@
   services.gnome.gnome-keyring.enable = true;
   # services.dbus.enable = true;
 
+  services.gvfs.enable = true; # Mount, trash, and other functionalities
+  services.tumbler.enable = true; # Thumbnail support for images
+
   xdg.portal = {
     enable = true;
     wlr.enable = true;
