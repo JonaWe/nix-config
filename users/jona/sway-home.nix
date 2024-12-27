ae0e8399eefa1c0ff349b6877b21a7fc7185ce41
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-27 02:29:33 +0100

    adds a bunch more options for sway

--- users/jona/sway-home.nix
+++ users/jona/sway-home.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }:let
   mod = "Mod4";
@@ -41,20 +42,107 @@ in {
 # wlr.enable = true;
     extraPortals = [pkgs.xdg-desktop-portal-gtk];
   };
+
   wayland.windowManager.sway = {
       enable = true;
+
       config = rec {
           modifier = "Mod4";
+          terminal = "kitty";
           bars = [{
-            command = "${pkgs.waybar}/bin/waybar";
+              command = "${pkgs.waybar}/bin/waybar";
           }];
+          fonts = {
+            names = ["FiraCode Nerd Font"];
+            size = 12;
+          };
+          input = {
+            "type:keyboard" = {
+                xkb_layout = "gcj";
+                xkb_options = "caps:ctrl_modifier";
+                repeat_delay = 1000;
+                repeat_rate = 25;
+            };
+            "type:touchpad" = {
+                tap = "enable";
+                natural_scroll = "enable";
+                pointer_accel = "0.25";
+                scroll_factor = "0.5";
+            };
+          };
+          output = {
+              DP-1 = {
+                  mode = "2560x1440@144Hz";
+              };
+          };
           focus = {
               forceWrapping = false;
               followMouse = false;
           };
+          gaps = {
+              inner = 0;
+              outer = 0;
+          };
+          window = {
+              border = 1;
+              titlebar = false;
+          };
+          floating = {
+            border = 1;
+            titlebar = false;
+          };
+          keybindings = lib.mkOptionDefault {
+            "${mod}+q" = "kill";
+            "${mod}+Return" = "exec ${pkgs.kitty}/bin/kitty";
+            "${mod}+d" = "exec ${pkgs.rofi-wayland}/bin/rofi -show drun -show-icons";
+            "${mod}+Shift+d" = "exec ${pkgs.rofi-wayland}/bin/rofi -show calc -modi calc -no-show-match -no-sort";
+            "${mod}+e" = "exec ${pkgs.pcmanfm}/bin/pcmanfm";
+            "${mod}+g" = "exec ${pkgs.swaynotificationcenter}/bin/swaync-client -t -sw";
+            "${mod}+g" = "exec ${pkgs.swaynotificationcenter}/bin/swaync-client -t -sw";
+
+            "${mod}+${left}" = "focus left";
+            "${mod}+${right}" = "focus right";
+            "${mod}+${up}" = "focus up";
+            "${mod}+${down}" = "focus up";
+            "${mod}+Shift+${left}" = "move left";
+            "${mod}+Shift+${right}" = "move right";
+            "${mod}+Shift+${up}" = "move up";
+            "${mod}+Shift+${down}" = "move up";
+            "${mod}+b" = "workspace back_and_forth";
+            # alt tab
+            "Mod1+Tab" = "workspace back_and_forth";
+            "${mod}+Shift+b" = "move container to workspace back_and_forth; workspace back_and_forth";
+
+            "${mod}+Shift+v" = "split v";
+            "${mod}+f" = "fullscreen toggle";
+            "${mod}+Shift+Space" = "floating toggle";
+            "${mod}+Space" = "focus mode_toggle";
+            "${mod}+Shift+s" = "sticky toggle";
+            "${mod}+Shift+c" = "reload";
+
+            "${mod}+Ctrl+l" = "layout toggle split tabbed";
+
+            "XF86AudioRaiseVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ +5%";
+            "XF86AudioLowerVolume" = "exec --no-startup-id pactl set-sink-volume @DEFAULT_SINK@ -5%";
+            "XF86AudioMute" = "exec --no-startup-id pactl set-sink-mute @DEFAULT_SINK@ toggle";
+            "XF86AudioMicMute" = "exec --no-startup-id pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+            "XF86AudioPlay" = "exec --no-startup-id playerctl play-pause";
+            "XF86AudioNext" = "exec --no-startup-id playerctl next";
+            "XF86AudioPrev" = "exec --no-startup-id playerctl previous";
+            "XF86MonBrightnessUp" = "exec --no-startup-id brightnessctl set 10%+";
+            "XF86MonBrightnessDown" = "exec --no-startup-id brightnessctl set 10%-";
+
+            "${mod}+1" = "workspace 1";
+            "${mod}+2" = "workspace 2";
+            "${mod}+3" = "workspace 3";
+            "${mod}+4" = "workspace 4";
+            "${mod}+5" = "workspace 5";
+            "${mod}+6" = "workspace 6";
+            "${mod}+7" = "workspace 7";
+          };
       };
-      extraOptions = [  "--unsupported-gpu"
-      ];
+
+      extraOptions = [ "--unsupported-gpu" ];
       swaynag.enable = true;
       extraSessionCommands = ''
           export SDL_VIDEODRIVER=wayland
@@ -65,6 +153,10 @@ in {
           '';
       wrapperFeatures.gtk = true;
   };
+
+
+
+
   # programs.sway = {
   #   enable = true;
   #
