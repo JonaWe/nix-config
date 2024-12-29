6883364073511a6dcd43d165e6864f3d4edda73d
Jona <jona.wessendorf1@uni-bielefeld.de>
2024-12-29 01:31:34 +0100

    adds gtk options

--- users/jona/sway-home.nix
+++ users/jona/sway-home.nix
@@ -207,8 +207,24 @@ in {
       wrapperFeatures.gtk = true;
   };
 
+  gtk = {
+    enable = true;
 
+    theme = {
+      package = pkgs.flat-remix-gtk;
+      name = "Flat-Remix-GTK-Grey-Darkest";
+    };
 
+    iconTheme = {
+      package = pkgs.gnome.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+
+    font = {
+      name = "Sans";
+      size = 11;
+    };
+  };
 
   # programs.sway = {
   #   enable = true;
