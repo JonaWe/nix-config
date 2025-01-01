80a7346cb25abc39ea4477fe6a7d45b29d13737e
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-02 00:36:36 +0100

    adds mods to server

--- modules/minecraft-servers.nix
+++ modules/minecraft-servers.nix
@@ -12,11 +12,16 @@
     servers = {
       test-server = {
         enable = true;
-        package = pkgs.vanillaServers.vanilla-1_21_4;
+        package = pkgs.fabricServers.fabric-1_21_4;
         serverProperties = {
           gamemode = "survival";
           difficulty = "hard";
           simulation-distance = 10;
+        };
+        symlinks = {
+          "mods" = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
+            fabric-api = fetchurl {url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/5tj7y3PJ/fabric-api-0.114.0%2B1.21.4.jar";};
+          });
         };
       };
     };
