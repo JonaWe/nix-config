8fdff170b35c7079485c28a8aa4f2a472fb3a871
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-02 00:06:06 +0100

    fixes typo

--- modules/minecraft-servers.nix
+++ modules/minecraft-servers.nix
@@ -6,7 +6,7 @@
   imports = [inputs.nix-minecraft.nixosModules.minecraft-servers];
   nixpkgs.overlays = [inputs.nix-minecraft.overlay];
 
-  services.minecraft-server = {
+  services.minecraft-servers = {
     enable = true;
     eula = true;
     servers = {
