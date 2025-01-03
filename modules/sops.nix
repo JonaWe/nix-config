5bd59c594fbb0bf99e1fe5feb2ef4e7c901742af
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-03 20:01:28 +0100

    adds duckdns

--- modules/sops.nix
+++ modules/sops.nix
@@ -13,34 +13,35 @@
 
   sops.age.keyFile = "/home/jona/.config/sops/age/keys.txt";
 
-  sops.secrets.example_key = {};
-  sops.secrets."myservice/my_subdir/my_secret" = {
-    owner = "sometestservice";
-  };
-
-  systemd.services."sometestservice" = {
-    script = ''
-      echo "
-      Hey bro! I'm a service, and imma send this secure password:
-      $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
-      located in:
-      ${config.sops.secrets."myservice/my_subdir/my_secret".path}
-      to database and hack the mainframe
-      " > /var/lib/sometestservice/testfile
-    '';
-    serviceConfig = {
-      User = "sometestservice";
-      WorkingDirectory = "/var/lib/sometestservice";
-    };
-  };
-
-  users.users.sometestservice = {
-    home = "/var/lib/sometestservice";
-    createHome = true;
-    isSystemUser = true;
-    group = "sometestservice";
-  };
-  users.groups.sometestservice = {};
+  sops.secrets."duckdns/token" = {};
+  # sops.secrets.example_key = {};
+  # sops.secrets."myservice/my_subdir/my_secret" = {
+  #   owner = "sometestservice";
+  # };
+  #
+  # systemd.services."sometestservice" = {
+  #   script = ''
+  #     echo "
+  #     Hey bro! I'm a service, and imma send this secure password:
+  #     $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
+  #     located in:
+  #     ${config.sops.secrets."myservice/my_subdir/my_secret".path}
+  #     to database and hack the mainframe
+  #     " > /var/lib/sometestservice/testfile
+  #   '';
+  #   serviceConfig = {
+  #     User = "sometestservice";
+  #     WorkingDirectory = "/var/lib/sometestservice";
+  #   };
+  # };
+  #
+  # users.users.sometestservice = {
+  #   home = "/var/lib/sometestservice";
+  #   createHome = true;
+  #   isSystemUser = true;
+  #   group = "sometestservice";
+  # };
+  # users.groups.sometestservice = {};
 
   environment.systemPackages = with pkgs; [
     sops
