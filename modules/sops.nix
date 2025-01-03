3b5ab37485fda4f9be337bd113b6cd3df7c3398d
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-04 00:00:08 +0100

    duckdns setup part 1

--- modules/sops.nix
+++ modules/sops.nix
@@ -13,27 +13,39 @@
 
   sops.age.keyFile = "/home/jona/.config/sops/age/keys.txt";
 
-  sops.secrets."duckdns/token" = {};
+  sops.secrets."duckdns/token" = {
+    owner = "duckdns";
+  };
+
+  users.users.duckdns = {
+    isSystemUser = true;
+  };
+
+  systemd.services."sometestservice" = {
+    path = [
+      pkgs.curl
+    ];
+    script = ''
+      echo "
+      Hey bro! I'm a service, and imma send this secure password:
+      $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
+      located in:
+      ${config.sops.secrets."myservice/my_subdir/my_secret".path}
+      to database and hack the mainframe
+      " > /var/lib/sometestservice/testfile
+    '';
+    startAt = "hourly";
+    serviceConfig = {
+      User = "duckdns";
+      WorkingDirectory = "/var/lib/duckdns";
+    };
+  };
+
   # sops.secrets.example_key = {};
   # sops.secrets."myservice/my_subdir/my_secret" = {
   #   owner = "sometestservice";
   # };
   #
-  # systemd.services."sometestservice" = {
-  #   script = ''
-  #     echo "
-  #     Hey bro! I'm a service, and imma send this secure password:
-  #     $(cat ${config.sops.secrets."myservice/my_subdir/my_secret".path})
-  #     located in:
-  #     ${config.sops.secrets."myservice/my_subdir/my_secret".path}
-  #     to database and hack the mainframe
-  #     " > /var/lib/sometestservice/testfile
-  #   '';
-  #   serviceConfig = {
-  #     User = "sometestservice";
-  #     WorkingDirectory = "/var/lib/sometestservice";
-  #   };
-  # };
   #
   # users.users.sometestservice = {
   #   home = "/var/lib/sometestservice";
