e7f2f78ff2ec8c1a545799a0ec9ef5d5bbb346d3
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-06 22:26:07 +0100

    adds todo command for variable uid and gid

--- hosts/pangolin/default.nix
+++ hosts/pangolin/default.nix
@@ -28,6 +28,7 @@
     fsType = "cifs";
     options = let
       # this line prevents hanging on network split
+      # TODO: replace uid and gid with variables
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users,uid=1000,gid=100";
     # in ["${automount_opts},credentials=/etc/nixos/smb-secrets"];
     in ["${automount_opts}"];
