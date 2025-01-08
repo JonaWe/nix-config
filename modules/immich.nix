e6d509f1042dc0640242109e498d4b95059f3c05
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-08 12:30:44 +0100

    adds default port for immich

--- modules/immich.nix
+++ modules/immich.nix
@@ -8,14 +8,13 @@
 
   services.immich = {
     enable = true;
-    # openFirewall = true;
+    openFirewall = true;
     host = "192.168.188.133";
-    port = 3000;
+    port = 2283;
     user = "immich";
     group = "immich";
   };
 
-  networking.firewall.allowedTCPPorts = [ 3000 ];
 
   systemd.tmpfiles.rules = [
     "d /data/media/immich 0700 ${toString config.services.immich.user} ${toString config.services.immich.group} -"
