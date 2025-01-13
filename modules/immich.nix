f06c0ff3af0c07004c2d1d06dcc42c98e4fedbc3
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-13 13:28:03 +0100

    adjusts immich host

--- modules/immich.nix
+++ modules/immich.nix
@@ -9,16 +9,16 @@
   services.immich = {
     enable = true;
     openFirewall = true;
-    host = "127.0.0.1";
+    host = "0.0.0.0";
     port = 2283;
     mediaLocation = "/data/media/immich";
     user = "immich";
     group = "immich";
     settings.server.externalDomain = "https://photos.home.pinkorca.de";
-    environment = {
-      IMMICH_ENV = "production";
-      IMMICH_LOG_LEVEL = "log";
-    };
+    # environment = {
+    #   IMMICH_ENV = "production";
+    #   IMMICH_LOG_LEVEL = "log";
+    # };
   };
 
   systemd.tmpfiles.rules = [
