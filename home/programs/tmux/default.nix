25bc792ffffe2d0b38b1265558b7cd75a042050a
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-14 16:35:44 +0100

    uses tmux homemanager package for config

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: {
   # home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
-  home.packages = with pkgs; [
-    tmux
-    tmuxPlugins.session-wizard
-  ];
+  # home.packages = with pkgs; [
+  #   tmux
+  #   tmuxPlugins.session-wizard
+  # ];
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    clock24 = true;
+    disableConfirmationPrompt = true;
+  };
 }
