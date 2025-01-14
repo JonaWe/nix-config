9014a8f79e6bd58a9de6cd876b3900c061c0165f
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-14 16:31:07 +0100

    tmux default settings

--- home/programs/tmux/default.nix
+++ home/programs/tmux/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
+  # home.file.".config/tmux/tmux.conf".source = ./tmux.conf;
   home.packages = with pkgs; [
     tmux
     tmuxPlugins.session-wizard
