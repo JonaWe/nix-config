5e4af9406b89c017268258c6e56db79d996ad5f7
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-16 12:37:13 +0100

    updates devices

--- modules/syncting-server.nix
+++ modules/syncting-server.nix
@@ -59,13 +59,16 @@
           phone = {
             id = "NWDFWNP-J265KD5-TG6JSA6-5VWRPIN-PN3IBIW-2TUWQ2D-WCKAZ3Q-ZGQ7LAN";
           };
+          homelab = {
+            id = "DP6G2PA-QLRCJRJ-SZ644M2-LVX2T47-33KGVYT-2QJXS7N-JFXGNZ5-IIVC5Q5";
+          };
         };
         folders = {
           "test-folder" = {
             id = "test-folder";
             label = "Test Folder";
             path = "/home/jona/test-folder";
-            devices = ["pangolin" "phone"];
+            devices = ["pangolin" "phone" "homelab"];
           };
         };
       };
