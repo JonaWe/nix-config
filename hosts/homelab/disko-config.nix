3ba60adf6fc21bc9a0c183605be2303a55c36e8a
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-19 00:03:12 +0100

    diko first refactor to remove repetition

--- hosts/homelab/disko-config.nix
+++ hosts/homelab/disko-config.nix
@@ -1,106 +1,84 @@
-# /dev/disk/by-id/ata-SanDisk_SDSSDA240G_163623441801
-# /dev/disk/by-id/nvme-Samsung_SSD_980_500GB_S64DNL0T513845T
-# /dev/disk/by-id/wwn-0x50004cf206c95bfa
 {
-  disko.devices = {
-    disk = {
-      nvme = {
-        type = "disk";
-        device = "/dev/disk/by-id/nvme-Samsung_SSD_980_500GB_S64DNL0T513845T";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "1024M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
+  disko.devices = let
+    dataDrives = [
+      "/dev/disk/by-id/ata-ST16000NM001G-2KK103_WL20TJNQ"
+      "/dev/disk/by-id/ata-ST16000NM001G-2KK103_ZL2NZAQ8"
+      "/dev/disk/by-id/ata-ST16000NM001G-2KK103_WL20VP3M"
+    ];
+    dataDiskConfig = builtins.listToAttrs (builtins.map (drive: {
+        name = "dataDrive:" + drive;
+        value = {
+          type = "disk";
+          device = drive;
+          content = {
+            type = "gpt";
+            partitions = {
+              files = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zdata";
+                };
               };
             };
           };
         };
-      };
-      data = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST16000NM001G-2KK103_WL20TJNQ";
-        content = {
-          type = "gpt";
-          partitions = {
-            files = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zdata";
-              };
-            };
-          };
-        };
-      };
-      data2 = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST16000NM001G-2KK103_WL20VP3M";
-        content = {
-          type = "gpt";
-          partitions = {
-            files = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zdata";
-              };
-            };
-          };
-        };
-      };
-      data3 = {
-        type = "disk";
-        device = "/dev/disk/by-id/ata-ST16000NM001G-2KK103_ZL2NZAQ8";
-        content = {
-          type = "gpt";
-          partitions = {
-            files = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zdata";
-              };
-            };
-          };
-        };
-      };
+      })
+      dataDrives);
+    rootFsOptions = {
+      acltype = "posixacl";
+      atime = "off";
+      canmount = "off";
+      compression = "zstd";
+      "com.sun:auto-snapshot" = "false";
+      dnodesize = "auto";
+      normalization = "formD";
+      relatime = "on";
+      xattr = "sa";
     };
+  in {
+    disk =
+      {
+        nvme = {
+          type = "disk";
+          device = "/dev/disk/by-id/nvme-Samsung_SSD_980_500GB_S64DNL0T513845T";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                priority = 1;
+                name = "ESP";
+                start = "1M";
+                end = "1024M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zroot";
+                };
+              };
+            };
+          };
+        };
+      }
+      // dataDiskConfig;
     zpool = {
       zroot = {
         type = "zpool";
-        options.cachefile = "none";
         mode = "";
         options = {
           ashift = "12";
           autotrim = "on";
+          cachefile = "none";
         };
-        rootFsOptions = {
-          acltype = "posixacl";
-          atime = "off";
-          canmount = "off";
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "false";
-          dnodesize = "auto";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-        };
+        inherit rootFsOptions;
         datasets = {
           "root" = {
             mountpoint = "/";
@@ -136,17 +114,7 @@
           ashift = "12";
           autotrim = "on";
         };
-        rootFsOptions = {
-          acltype = "posixacl";
-          atime = "off";
-          canmount = "off";
-          compression = "zstd";
-          "com.sun:auto-snapshot" = "false";
-          dnodesize = "auto";
-          normalization = "formD";
-          relatime = "on";
-          xattr = "sa";
-        };
+        inherit rootFsOptions;
         datasets = {
           encrypted = {
             type = "zfs_fs";
