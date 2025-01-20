360e0febb9f45a3bb875aeaf529fba314d66e6ef
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-20 18:50:15 +0100

    adds tumbler

--- modules/sway.nix
+++ modules/sway.nix
@@ -12,24 +12,24 @@
     '';
   };
 
-  configure-gtk = pkgs.writeTextFile {
-    name = "configure-gtk";
-    destination = "/bin/configure/-gtk";
-    executable = true;
-    text = let
-      schema = pkgs.gsettings-desktop-schemas;
-      datadir = "${schema}/share/gsetting-schemas/${schema.name}";
-    in ''
-      export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
-      gnome_schema=org.gnome.desktop.interface
-      gsettings set $gnome_schema gtk-theme 'WhiteSur-dark'
-      gsettings set $gnome_schema cursor-theme 'capitaine-cursors-white'
-    '';
-  };
+  # configure-gtk = pkgs.writeTextFile {
+  #   name = "configure-gtk";
+  #   destination = "/bin/configure/-gtk";
+  #   executable = true;
+  #   text = let
+  #     schema = pkgs.gsettings-desktop-schemas;
+  #     datadir = "${schema}/share/gsetting-schemas/${schema.name}";
+  #   in ''
+  #     export XDG_DATA_DIRS=${datadir}:$XDG_DATA_DIRS
+  #     gnome_schema=org.gnome.desktop.interface
+  #     gsettings set $gnome_schema gtk-theme 'WhiteSur-dark'
+  #     gsettings set $gnome_schema cursor-theme 'capitaine-cursors-white'
+  #   '';
+  # };
 in {
   environment.systemPackages = [
     dbus-sway-environment
-    configure-gtk
+    # configure-gtk
   ];
 
   security.polkit.enable = true;
@@ -39,6 +39,7 @@ in {
   services.gvfs.enable = true;
   services.udisks2.enable = true;
   services.devmon.enable = true;
+  services.tumbler.enable = true;
 
   programs.dconf.enable = true;
   programs.sway.enable = true;
