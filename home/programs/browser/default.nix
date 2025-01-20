4dd7037704b1d19ac855b6b4e6ed54e485330d73
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-20 20:03:51 +0100

    config cleanup and modulasaiton

--- home/programs/browser/default.nix
+++ home/programs/browser/default.nix
@@ -1,5 +1,4 @@
 {pkgs, ...}: {
-  # home.file.".config/kitty/kitty.conf".source = ./kitty.conf;
   # home.packages = with pkgs; [floorp firefox];
   programs.chromium = {
     enable = true;
---
+++ home/programs/sway/themes.nix
@@ -0,0 +1,41 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.pointerCursor = {
+    name = "Banana";
+    size = 32;
+    package = pkgs.banana-cursor;
+    x11.enable = true;
+    gtk.enable = true;
+  };
+
+  gtk = {
+    enable = true;
+    theme.name = "Adwaita-dark";
+    theme.package = pkgs.gnome-themes-extra;
+    iconTheme.package = pkgs.adwaita-icon-theme;
+    iconTheme.name = "Adwaita";
+    cursorTheme.name = config.home.pointerCursor.name;
+    cursorTheme.size = config.home.pointerCursor.size;
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
+
+  qt = {
+    enable = true;
+    style.name = "adwaita-dark";
+    style.package = pkgs.adwaita-qt;
+  };
+}
