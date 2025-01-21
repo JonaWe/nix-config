87d310e21cf3c212d6475230be05d219d1c3427e
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-01-21 23:09:31 +0100

    enables fail2ban and disables password login

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -2,14 +2,14 @@
   services.openssh = {
     enable = true;
     settings = {
-      # PasswordAuthentication = false;
-      PermitRootLogin = "yes";
-      # PermitRootLogin = "prohibit-password";
-      PasswordAuthentication = true;
+      PasswordAuthentication = false;
+      # PermitRootLogin = "yes";
+      PermitRootLogin = "prohibit-password";
+      # PasswordAuthentication = true;
     };
   };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjGQP+a5gpUSfha442tu7tTW7rhnp6Mh1e30rR7id+F jona@arch-jona"
   ];
-  # services.fail2ban.enable = true;
+  services.fail2ban.enable = true;
 }
