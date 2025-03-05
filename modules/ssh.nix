209bcc5de8feb51b3c58fbc66be1e258191dbec5
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-03-05 14:24:46 +0100

    ignores local ip subnet for fail2ban

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -11,5 +11,8 @@
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjGQP+a5gpUSfha442tu7tTW7rhnp6Mh1e30rR7id+F jona@arch-jona"
   ];
-  services.fail2ban.enable = true;
+  services.fail2ban = {
+    enable = true;
+    ignoreIP = ["192.168.188.0/24"];
+  };
 }
