c2e55a8bd6e4732511ea13fe9c8154c68b158745
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-03-06 14:23:28 +0100

    disables agent forwarding for git ssh user

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -7,6 +7,14 @@
       PermitRootLogin = "prohibit-password";
       # PasswordAuthentication = true;
     };
+    extraConfig = ''
+      Match user git
+        AllowTcpForwarding no
+        AllowAgentForwarding no
+        PasswordAuthentication no
+        PermitTTY no
+        X11Forwarding no
+    '';
   };
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBjGQP+a5gpUSfha442tu7tTW7rhnp6Mh1e30rR7id+F jona@arch-jona"
