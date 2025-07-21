541eca2c41f8a5b8ac01f583bd20ab4c41631e57
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-07-22 01:41:49 +0200

    removes paperless-ai

--- modules/services/paperless.nix
+++ modules/services/paperless.nix
@@ -52,54 +52,54 @@ in {
       };
     };
 
-    services.nginx.virtualHosts."paperless-ai.ts.pinkorca.de" = lib.mkIf config.myconf.services.nginx.enable {
-      useACMEHost = "pinkorca.de";
-      forceSSL = true;
-      locations."/" = {
-        proxyPass = "http://localhost:3000/";
-      };
-    };
-
-    users.users.paperless-ai = {
-      isNormalUser = true;
-      home = "/var/lib/paperless-ai";
-      group = "paperless-ai";
-      description = "User for paperless-ai";
-      uid = 133710;
-    };
-
-    users.groups.paperless-ai = {
-      gid = 133710;
-    };
-
-    virtualisation.oci-containers.containers."paperless-ai" = {
-      image = "clusterzx/paperless-ai";
-      volumes = [
-        "/var/lib/paperless-ai:/app/data"
-      ];
-      environment = {
-        "PGID" = "133710";
-        "PUID" = "133710";
-        "PAPERLESS_AI_PORT" = "3000";
-        "RAG_SERVICE_URL" = "http://localhost:8000";
-        "RAG_SERVICE_ENABLED" = "true";
-      };
-      # ports = [
-      # "127.0.0.1:3033:3033"
-      # "127.0.0.1:3033:3000"
-      # "28981"
-      # "11434"
-      # ];
-      log-driver = "journald";
-      extraOptions = [
-        "--network=host"
-        "--pull=always"
-      ];
-    };
-
-    systemd.tmpfiles.rules = [
-      "d /var/lib/paperless-ai 0700 paperless-ai paperless-ai -"
-    ];
+    # services.nginx.virtualHosts."paperless-ai.ts.pinkorca.de" = lib.mkIf config.myconf.services.nginx.enable {
+    #   useACMEHost = "pinkorca.de";
+    #   forceSSL = true;
+    #   locations."/" = {
+    #     proxyPass = "http://localhost:3000/";
+    #   };
+    # };
+    #
+    # users.users.paperless-ai = {
+    #   isNormalUser = true;
+    #   home = "/var/lib/paperless-ai";
+    #   group = "paperless-ai";
+    #   description = "User for paperless-ai";
+    #   uid = 133710;
+    # };
+    #
+    # users.groups.paperless-ai = {
+    #   gid = 133710;
+    # };
+    #
+    # virtualisation.oci-containers.containers."paperless-ai" = {
+    #   image = "clusterzx/paperless-ai";
+    #   volumes = [
+    #     "/var/lib/paperless-ai:/app/data"
+    #   ];
+    #   environment = {
+    #     "PGID" = "133710";
+    #     "PUID" = "133710";
+    #     "PAPERLESS_AI_PORT" = "3000";
+    #     "RAG_SERVICE_URL" = "http://localhost:8000";
+    #     "RAG_SERVICE_ENABLED" = "true";
+    #   };
+    #   # ports = [
+    #   # "127.0.0.1:3033:3033"
+    #   # "127.0.0.1:3033:3000"
+    #   # "28981"
+    #   # "11434"
+    #   # ];
+    #   log-driver = "journald";
+    #   extraOptions = [
+    #     "--network=host"
+    #     "--pull=always"
+    #   ];
+    # };
+    #
+    # systemd.tmpfiles.rules = [
+    #   "d /var/lib/paperless-ai 0700 paperless-ai paperless-ai -"
+    # ];
 
     # networking.firewall.allowedTCPPorts = [3000];
 
