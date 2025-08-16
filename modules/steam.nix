5bb4c4577ac291ac55f939302be3bf7a37cde6d8
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-08-16 18:13:40 +0200

    steam adjustments

--- modules/steam.nix
+++ modules/steam.nix
@@ -1,7 +1,18 @@
 {...}: {
-  programs.gamemode.enable = true;
-  programs.steam = {
-    enable = true;
-    localNetworkGameTransfers.openFirewall = true;
+  # programs.steam = {
+  #   enable = true;
+  #   localNetworkGameTransfers.openFirewall = true;
+  # };
+  programs = {
+    gamemode.enable = true;
+    gamescope = {
+      enable = true;
+      capSysNice = true;
+    };
+    steam = {
+      enable = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+    };
   };
 }
