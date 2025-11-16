71b6787a4cd6f27bb4df7decee07cfe105daceb2
Jona <jona.wessendorf1@uni-bielefeld.de>
2025-11-16 15:24:36 +0100

    renames zsh autosuggestion option

--- users/jona/base-home.nix
+++ users/jona/base-home.nix
@@ -114,7 +114,7 @@
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    enableAutosuggestions = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh.enable = true;
     plugins = [
