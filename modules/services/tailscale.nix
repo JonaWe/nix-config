9aa6226c42a736c307dda961e66dcae0afd4ae0d
Jona <jona.wessendorf1@uni-bielefeld.de>
2026-01-20 00:40:35 +0100

    simplifies tailscale config

--- modules/services/tailscale.nix
+++ modules/services/tailscale.nix
@@ -17,19 +17,9 @@ in {
       useRoutingFeatures = lib.mkIf cfg.exitNode "server";
     };
     networking.firewall = {
-      checkReversePath = if cfg.exitNode then false else "loose";
+      checkReversePath = "loose";
       trustedInterfaces = ["tailscale0"];
       allowedUDPPorts = [config.services.tailscale.port];
-      extraCommands = lib.mkIf cfg.exitNode ''
-        iptables -I FORWARD -i tailscale0 -j ACCEPT
-        iptables -I FORWARD -o tailscale0 -j ACCEPT
-        iptables -t nat -I POSTROUTING -o enp1s0 -j MASQUERADE
-      '';
-    };
-
-    boot.kernel.sysctl = lib.mkIf cfg.exitNode {
-      "net.ipv4.ip_forward" = 1;
-      "net.ipv6.conf.all.forwarding" = 1;
     };
 
     environment.systemPackages = [config.services.tailscale.package];
