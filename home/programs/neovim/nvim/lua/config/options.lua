eada37fceb290738991cba831ed9a2f68857d545
Jona <jonawe@users.noreply.github.com>
2026-04-12 16:59:55 +0200

    adds neovim clipboard osc52 support

--- home/programs/neovim/nvim/lua/config/options.lua
+++ home/programs/neovim/nvim/lua/config/options.lua
@@ -65,3 +65,15 @@ vim.filetype.add({
     image     = "systemd",
   },
 })
+
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
